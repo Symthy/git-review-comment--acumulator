615c135be54261e706366e675a94b88754293ee4
SYM <44676939+Symthy@users.noreply.github.com>
2022-09-28 15:00:59 +0000

    build: init

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+export default {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  features: {
+    storyStoreV7: true,
+    interactionsDebugger: true,
+  },
+
+  // vite.config.ts 再利用うまくいかなかったため直接指定
+  viteFinal: async (config) => {
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      src: path.resolve(__dirname, "../src"),
+    };
+    return config;
+  },
+};
